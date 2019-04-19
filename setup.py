ad1dbfd5287d3910bc61bcd4db4df045c3ca53ab
Sébastien Bourdeauducq <sb@m-labs.hk>
2019-04-19 23:12:05 +0800

    setup.py: add entry point

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="novatech409b",
     install_requires=["asyncserial"],
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "aqctl_novatech409b = novatech409b.aqctl_novatech409b:main",
+        ],
+    },
 )
